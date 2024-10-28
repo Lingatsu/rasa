430dcc5e4d132338bd0d815e0646d68f86182aa9
Lingatsu <115959500+Lingatsu@users.noreply.github.com>
2024-10-28 06:03:23 +0100

    Exercice 3 ne reconnait pas le lieu dans le action.py

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,28 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
+class ActionConfirmBooking(Action):
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def name(self) -> Text:
+        return "action_confirm_booking"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        date = tracker.get_slot('date')
+        location = tracker.get_slot('location')
+
+        if date and location:
+            message = f"Vous souhaitez réserver un hôtel à {location} pour le {date}, c'est bien cela ?"
+        else:
+            message = "Je n'ai pas compris les détails de votre réservation. Pouvez-vous répéter ?"
+
+        dispatcher.utter_message(text=message)
+
+        return []
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
